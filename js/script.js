648e393c5dbe06c018d3f9094cd9ce457e6d6932
Aleksandr Sannikov <alexander.v.sannikov@gmail.com>
2017-05-31 17:24:23 +0300

    Final build

--- js/script.js
+++ js/script.js
@@ -1,25 +1,33 @@
 'use strict';
 
 (function () {
-  var FIRST_NUMBER = 9;
-  var SECOND_NUMBER = 3;
-  var MAX_NUMBER = 20;
-  var MAX_LENGTH = 783;
-  var MAX_HEIGHT = 200;
-  var LEFT_PADDING = 34;
-  var INPUT_HALF_WIDTH = 12;
-  var INPUT_HEIGHT = 30;
+  /*
+   ** CHANGE NUMBERS HERE
+   */
+  var NUMBERS = {
+    FIRST: 6,
+    SECOND: 7
+  };
+
+  var STYLE_CONSTS = {
+    MAX_NUMBER: 20,
+    MAX_LENGTH: 783,
+    MAX_HEIGHT: 200,
+    LEFT_PADDING: 35,
+    INPUT_HALF_WIDTH: 12,
+    INPUT_HEIGHT: 30
+  };
 
   init();
 
   function init() {
-    var first = new Iteration(FIRST_NUMBER, 'first');
-    var second = new Iteration(SECOND_NUMBER, 'second');
+    var first = new Iteration(NUMBERS.FIRST, 'first');
+    var second = new Iteration(NUMBERS.SECOND, 'second');
 
     first.left = 0;
-    second.left = FIRST_NUMBER;
+    second.left = NUMBERS.FIRST;
 
-    if (isCorrect()) {
+    if (areNumbersCorrect()) {
       renderExpression(first.digit, second.digit, first.card, second.card);
 
       first.renderArrow(
@@ -30,7 +38,7 @@
         )
       );
     } else {
-      console.log('Значения находятся за пределами допустимого диапазона');
+      alert('Значения находятся за пределами допустимого диапазона');
     }
   }
 
@@ -46,9 +54,9 @@
     this.renderArrow = function (callback) {
       this.arrow.classList.remove('invisible');
 
-      this.arrow.style.left = (this.left * MAX_LENGTH / MAX_NUMBER + LEFT_PADDING) + 'px';
-      this.arrow.style.width = (this.digit * MAX_LENGTH / MAX_NUMBER) + 'px';
-      this.arrow.style.height = (this.digit * MAX_HEIGHT / MAX_NUMBER) + 'px';
+      this.arrow.style.left = (this.left * STYLE_CONSTS.MAX_LENGTH / STYLE_CONSTS.MAX_NUMBER + STYLE_CONSTS.LEFT_PADDING) + 'px';
+      this.arrow.style.width = (this.digit * STYLE_CONSTS.MAX_LENGTH / STYLE_CONSTS.MAX_NUMBER) + 'px';
+      this.arrow.style.height = (this.digit * STYLE_CONSTS.MAX_HEIGHT / STYLE_CONSTS.MAX_NUMBER) + 'px';
 
       callback();
     }
@@ -66,11 +74,7 @@
 
       digitInput.addEventListener('change', function (evt) {
         if (+evt.target.value === digit) {
-          digitInput.classList.add('invisible');
-
-          answer.classList.remove('invisible');
-          answer.innerText = digit;
-
+          changeInputToAnswer(digitInput, answer, digit);
           card.classList.remove('card__number--highlight');
 
           callback();
@@ -85,8 +89,8 @@
     }
   }
 
-  function isCorrect() {
-    return FIRST_NUMBER >= 6 && FIRST_NUMBER <= 9 && FIRST_NUMBER + SECOND_NUMBER >= 11 && FIRST_NUMBER + SECOND_NUMBER <= 14;
+  function areNumbersCorrect() {
+    return NUMBERS.FIRST >= 6 && NUMBERS.FIRST <= 9 && NUMBERS.FIRST + NUMBERS.SECOND >= 11 && NUMBERS.FIRST + NUMBERS.SECOND <= 14;
   }
 
   function renderExpression(firstNumber, secondNumber, firstContainer, secondContainer) {
@@ -95,8 +99,8 @@
   }
 
   function stylize(obj, num) {
-    var left = (num * MAX_LENGTH / MAX_NUMBER) / 2 - INPUT_HALF_WIDTH;
-    var bottom = num * MAX_HEIGHT / MAX_NUMBER + INPUT_HEIGHT;
+    var left = (num * STYLE_CONSTS.MAX_LENGTH / STYLE_CONSTS.MAX_NUMBER) / 2 - STYLE_CONSTS.INPUT_HALF_WIDTH;
+    var bottom = num * STYLE_CONSTS.MAX_HEIGHT / STYLE_CONSTS.MAX_NUMBER + STYLE_CONSTS.INPUT_HEIGHT;
 
     obj.classList.add('card__input');
     obj.style.left = left + 'px';
@@ -106,7 +110,7 @@
   function getFinalResult() {
     var expression = document.querySelector('.card__expression');
     var answer = document.querySelector('.card__answer');
-    var result = FIRST_NUMBER + SECOND_NUMBER;
+    var result = NUMBERS.FIRST + NUMBERS.SECOND;
     var digitInput = document.createElement('input');
 
     digitInput.classList.add('card__answer');
@@ -114,15 +118,18 @@
 
     digitInput.addEventListener('change', function (evt) {
       if (+evt.target.value === result) {
-        digitInput.classList.add('invisible');
-
-        answer.classList.remove('invisible');
-        answer.innerText = result;
+        changeInputToAnswer(digitInput, answer, result);
       } else {
         digitInput.classList.add('invalid');
       }
     });
 
     expression.appendChild(digitInput);
+  }
+
+  function changeInputToAnswer(input, answer, num) {
+    input.classList.add('invisible');
+    answer.classList.remove('invisible');
+    answer.innerText = num;
   }
 })();
